26d6acd77423961389bc43ebbb907ed3b25206cc
Anastasiya <anastasiya.pospel@mail.ru>
2022-08-24 13:16:26 +0300

    task 13

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int number;
+while (true) {
+    Console.Write("Введите трёхзначное число: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number < 100) {
+        Console.WriteLine("В введённом числе нет третьей цифры. Поробуйте ещё раз.");
+    }
+    else break;
+}
+
+string strNumber = System.Convert.ToString(number);
+Console.WriteLine(strNumber[1]);
